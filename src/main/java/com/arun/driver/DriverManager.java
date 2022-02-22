668997c56434dc51d77c5fffba6d039a7c274fa0
Arun <aneja.arun1@gmail.com>
2022-02-22 15:04:56 +0530

    Adding Driver abstraction

---
+++ src/main/java/com/arun/driver/DriverManager.java
@@ -0,0 +1,50 @@
+package com.arun.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverManager {
+  
+  private String browserNameProperty = "browserName";
+  private String browserName = System.getProperty(browserNameProperty);
+  
+  public WebDriver open() {
+    
+    boolean isBrowserPropertySet = (browserName == null) ? false : true;
+    
+    if (!isBrowserPropertySet) {
+      setDefaultBrowser();
+    }
+ 
+    switch(System.getProperty(browserNameProperty).trim().toLowerCase()) {
+    case "firefox":
+      WebDriverManager.firefoxdriver().setup();
+      return new FirefoxDriver();
+    case "edge":
+      WebDriverManager.edgedriver().setup();
+      return new EdgeDriver();
+    case "chrome":
+    default:
+      WebDriverManager.chromedriver().setup();
+      return new ChromeDriver();
+    
+    }
+  }
+  
+  public void setDefaultBrowser() {
+    setBrowser("default");
+  }
+  
+  public void setBrowser(String browserName) {
+    System.setProperty(browserNameProperty, browserName);
+  }
+  
+  public static void main(String args[]) {
+    new DriverAbstraction().open().get("http://www.yahoo.com");
+  }
+
+}
